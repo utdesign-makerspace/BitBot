42a31442a74ef18f7cbcbf30683816b3e61865a3
Cameron Steele <cams@Camerons-MBP.localdomain>
2021-10-26 19:33:23 -0500

    Added Linting w/ Prettier

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.login(DISCORD_TOKEN);
