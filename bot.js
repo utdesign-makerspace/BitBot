628c1f974f16ac65d480bfa8d362e227235c2c7b
Hudson Samuels <hudson.samuels@gmail.com>
2021-10-26 19:25:19 -0500

    Implementing Discord.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const { DISCORD_TOKEN } = process.env;
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(DISCORD_TOKEN);
