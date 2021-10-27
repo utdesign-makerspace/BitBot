c5b539b799ceb5288603e6b9236caf59d35b5a27
Cameron Steele <cam.steeleis@gmail.com>
2021-10-26 22:33:30 -0500

    Rewrote some code in a more ES6 way

--- bot.js
+++ bot.js
@@ -9,41 +9,37 @@ const mongoose = require('mongoose');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'))
+  .forEach((file) => {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+  });
 
 client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+  const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+  if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true
+    });
+  }
 });
-
-mongoose.connect(MONGODB_SRV, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log("Connected to the database.");
-}).catch((err)=> {
-    console.log(err);
-})
 
 client.login(DISCORD_TOKEN);
--- deploy-commands.js
+++ deploy-commands.js
@@ -5,29 +5,31 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	if (command.context)
-		commands.push(command.data);
-	else
-		commands.push(command.data.toJSON());
-}
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'))
+  .forEach((file) => {
+    const command = require(`./commands/${file}`);
+    if (command.context) commands.push(command.data);
+    else commands.push(command.data.toJSON());
+  });
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
-	try {
-		console.log('Started refreshing application (/) guild commands.');
+  try {
+    console.log('Started refreshing application (/) guild commands.');
 
-		await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-			{ body: commands },
-		);
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
 
-		console.log('Successfully reloaded application (/) guild commands.');
-	} catch (error) {
-		console.error(error);
-	}
+    console.log('Successfully reloaded application (/) guild commands.');
+  } catch (error) {
+    console.error(error);
+  }
 })();
