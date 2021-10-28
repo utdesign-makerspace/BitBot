b727315f0dfbec61836fc4b46e5a59b9d12fd76e
Cameron Steele <cam.steeleis@gmail.com>
2021-10-28 00:37:05 -0500

    Added support for all printers except Purple

--- commands/status.js
+++ commands/status.js
@@ -1,45 +1,83 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
 const constants = require('../lib/constants');
+const axios = require('axios');
+const https = require('https');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('status')
 		.setDescription('Gives the status of 3D printers.')
-        .addIntegerOption(option => 
-            option.setName("printer")
-                .setDescription("3D printer to check the status of")
-                .addChoices(constants.printerChoices)),
+		.addIntegerOption((option) =>
+			option
+				.setName('printer')
+				.setDescription('3D printer to check the status of')
+				.addChoices(constants.printerChoices)
+		),
 	ephemeral: true,
 	async execute(interaction) {
-        // Quickly check if we were NOT given a specific printer
-        if (interaction.options.getInteger('printer') == null) {
-            let statusEmbed = new Discord.MessageEmbed()
-                .setColor("#c1393d")
-                .setTitle(":information_source:  Printer Status")
-                .setFooter("Printers are first come, first served")
-                .setTimestamp();
-            return interaction.editReply({ embeds: [statusEmbed], ephemeral: true });
-        }
+		// Quickly check if we were NOT given a specific printer
+		if (interaction.options.getInteger('printer') == null) {
+			let statusEmbed = new Discord.MessageEmbed()
+				.setColor('#c1393d')
+				.setTitle(':information_source:  Printer Status')
+				.setFooter('Printers are first come, first served')
+				.setTimestamp();
+			return interaction.editReply({ embeds: [statusEmbed], ephemeral: true });
+		}
 
-        // Grab printer object from constants
-        const printer = constants.printers[interaction.options.getInteger('printer')];
+		// Grab printer object from constants
+		const printer =
+			constants.printers[interaction.options.getInteger('printer')];
 
-        // Create beginning of embed
-        const statusEmbed = new Discord.MessageEmbed()
-            .setColor(printer.color)
-			.setAuthor(printer.model)
+		// Create beginning of embed
+		const statusEmbed = new Discord.MessageEmbed()
+			.setColor(printer.color)
+			.setAuthor(printer.name + ' - ' + printer.model)
 			.setThumbnail(printer.thumbnail)
-            .setFooter("Printers are first come, first served")
-            .setTimestamp();
-        
-        // TODO: Add if statements to check if available, in use, or offline
-        if (true) { // example is available
-            statusEmbed.setTitle("🟢  Printer Available")
-                .addField("Available", "Yes", true);
-        }
+			.setFooter('Printers are first come, first served')
+			.setTimestamp();
 
-        // Send reply
+		// TODO: Add if statements to check if available, in use, or offline
+		const {
+			data: {
+				state: printerState,
+				progress: { printTimeLeft }
+			}
+		} = await axios({
+			method: 'get',
+			url: `http${printer.ssl ? 's' : ''}://${printer.ip}/api/job`,
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + printer.apikey
+			},
+			httpsAgent: new https.Agent({
+				rejectUnauthorized: false
+			})
+		});
+
+		if (printerState !== 'Printing') {
+			// example is available
+			statusEmbed
+				.setTitle('🟢  Printer Available')
+				.addField('Available', 'Yes', true);
+		} else {
+			// example is in use
+			statusEmbed
+				.setTitle('🟡  Printer In Use')
+				.addField(
+					'Available',
+					`<t:${Math.round(Date.now() / 1000) + printTimeLeft}:R>`,
+					false
+				)
+				.addField(
+					'Estimated to finish at',
+					`<t:${Math.round(Date.now() / 1000) + printTimeLeft}:f>`,
+					false
+				);
+		}
+
+		// Send reply
 		return interaction.editReply({ embeds: [statusEmbed], ephemeral: true });
-	},
+	}
 };
