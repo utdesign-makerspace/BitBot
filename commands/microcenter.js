6dedaedded73333e2baae47a197ebb6d80674e01
Cameron Steele <cams@Camerons-MBP.localdomain>
2021-10-29 15:05:07 -0500

    Added Basic lookup functionality

--- commands/microcenter.js
+++ commands/microcenter.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const {} = require('../lib/constants');
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -13,10 +13,27 @@ module.exports = {
     ),
   ephemeral: true,
   async execute(interaction) {
-    const data = await getProductByURL(
-      'https://www.microcenter.com/product/465902/c4labs-zebra-zero-raspberry-pi-zero-type-2-case-black-ice'
-    );
-    console.log(data);
-    return interaction.editReply('Done');
+    const { name, image, sku, qty, rating, price, url, instock } =
+      await getProductByURL(
+        'https://www.microcenter.com/product/465902/c4labs-zebra-zero-raspberry-pi-zero-type-2-case-black-ice'
+      );
+    const embed = new MessageEmbed()
+      .setTitle(name)
+      .setURL(url)
+      .setColor('#0099ff')
+      .addFields([
+        { name: 'Price', value: price, inline: true },
+        { name: 'Quanity', value: qty, inline: true },
+        { name: 'Rating', value: rating, inline: true },
+        { name: 'SKU', value: sku, inline: true }
+      ])
+      .setThumbnail(image)
+      .setAuthor('Microcenter', null, 'https://www.microcenter.com/')
+      .setFooter(`Data was last updated: ${Date.now()}`);
+    console.log('Sending');
+    return interaction.editReply({
+      embeds: [embed],
+      emphemeral: this.ephemeral
+    });
   }
 };
