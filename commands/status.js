6ea3cfa6355b8982f6136ed61c4551055c17989c
Hudson Samuels <hudson.samuels@gmail.com>
2021-10-31 14:28:30 -0500

    Added more flavor text, updated detailed view

--- commands/status.js
+++ commands/status.js
@@ -16,37 +16,54 @@ module.exports = {
 		),
 	ephemeral: true,
 	async execute(interaction) {
-        const printerID = interaction.options.getInteger('printer');
+		const printerID = interaction.options.getInteger('printer');
 
-        let msg;
-        
+		let msg;
+
 		if (printerID !== null) {
-            // Grab our message options
-            msg = await printers.getMessage(printerID, false);
-            // Create the buttons
-            const viewButton = new Discord.MessageButton({
-                customId: `${constants.status.detailsButtonId} ${printerID} 1`,
-                label: constants.status.showButtonText,
-                style: 'SECONDARY',
-            });
-            const cancelButton = new Discord.MessageButton({
-                customId: `${constants.status.cancelButtonId} ${printerID}`,
-                label: constants.status.cancelButtonText,
-                style: 'DANGER',
-                disabled: true,
-            });
-            // Allow stopping print if officer
-            if (interaction.member.roles.cache.some(role => role.name === constants.officerRoleName))
-                cancelButton.setDisabled(false);
-            const buttonRow = new Discord.MessageActionRow().addComponents(viewButton, cancelButton,);
-            msg.components = [buttonRow];
-        } else {
-            msg = await farm.getFarmEmbed();
-        }
+			// Grab our message options
+			msg = await printers.getMessage(printerID, false);
+			// Create the buttons
+			const refreshButton = new Discord.MessageButton({
+				customId: `${constants.status.detailsButtonId} ${printerID} 0`,
+				label: constants.status.refreshButtonText,
+				style: 'SECONDARY'
+			});
+			const viewButton = new Discord.MessageButton({
+				customId: `${constants.status.detailsButtonId} ${printerID} 1`,
+				label: constants.status.showButtonText,
+				style: 'SECONDARY'
+			});
+			const cancelButton = new Discord.MessageButton({
+				customId: `${constants.status.cancelButtonId} ${printerID}`,
+				label: constants.status.cancelButtonText,
+				style: 'DANGER',
+				disabled: true
+			});
+			// Allow stopping print if officer
+			if (
+				interaction.member.roles.cache.some(
+					(role) => role.name === constants.officerRoleName
+				)
+			)
+				cancelButton.setDisabled(false);
+			const buttonRow = new Discord.MessageActionRow().addComponents(
+				refreshButton,
+				viewButton
+			);
+			// Only add view and cancel buttons if printer in use
+			const data = await printers.getJob(printerID);
+			if (data && (data.state == 'Printing' || data.state == 'Paused'))
+				buttonRow.addComponents(cancelButton);
 
-        // Set ephemeral because we don't need everyone to see the status. We can change this once bot
-        // is live and we have a #bot-commands channel to avoid overflow.
-        msg.ephemeral = true;
-        interaction.editReply(msg);
+			msg.components = [buttonRow];
+		} else {
+			msg = await farm.getFarmEmbed();
+		}
+
+		// Set ephemeral because we don't need everyone to see the status. We can change this once bot
+		// is live and we have a #bot-commands channel to avoid overflow.
+		msg.ephemeral = true;
+		interaction.editReply(msg);
 	}
 };
