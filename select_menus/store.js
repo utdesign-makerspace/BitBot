ba7b92be3578d1ffc41172405b8f7319784b1411
Cameron Steele <cameronsteele@Camerons-Mini.localdomain>
2021-11-23 21:45:40 -0600

    Made menus work again

--- select_menus/store.js
+++ select_menus/store.js
@@ -1,23 +1,24 @@
 const Discord = require('discord.js');
-const constants = require('../lib/constants');
+//const constants = require('../lib/constants');
+const { getStoreItemByID } = require('../lib/store');
 const rewards = require('../lib/rewards');
 
 module.exports = {
 	id: 'store',
 	execute: async (interaction) => {
 		// Grab reward information
-		const rewardId = interaction.values[0];
-		const reward = constants.rewards[rewardId];
 
+		const rewardId = interaction.values[0];
+		const reward = await getStoreItemByID(rewardId);
 		// Check store embed
 		const rewardEmbed = new Discord.MessageEmbed()
 			.setColor('#c1393d')
 			.setAuthor('UTDesign Makerspace', 'https://i.imgur.com/lSwBDLb.png')
-			.setTitle(reward.name)
-			.setDescription(reward.description)
-			.addField('Price', `${reward.price} Bits`, true)
-			.addField('Pickup', reward.pickup, true)
-			.setThumbnail(reward.image);
+			.setTitle(reward.Title)
+			.setDescription(reward.Description)
+			.addField('Price', `${reward['Bit Price']} Bits`, true)
+			.addField('Pickup', reward.Pickup ? 'Yes' : 'No', true)
+			.setThumbnail(reward.Photo[0].url);
 
 		// Create button row
 		const redeemButton = new Discord.MessageButton({
