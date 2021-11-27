68fc2732f34633ad4fb6d65d1a57991cf6434b33
Cameron Steele <cam.steeleis@gmail.com>
2021-11-27 13:56:40 -0600

    Random rewards for messaging

--- events/message.js
+++ events/message.js
@@ -1,24 +1,25 @@
 const { arrayShuffle } = require('../lib/utils');
+const { giveUserPoints, getUserPoints } = require('../lib/bits');
 module.exports = {
 	event: 'messageCreate',
 	async execute(interaction) {
 		if (interaction.author.bot) return;
-		await awardPointForMessage(interaction);
+		console.log(interaction.content);
+		console.log(await getUserPoints(interaction.author.id));
 	}
 };
 
 const awardPointForMessage = async (interaction) => {
 	if (shouldMessageBeAwarded(interaction.author.id)) {
+		giveUserPoints(interaction.author.id, 0.1);
 		interaction.react('🪙');
 	}
 };
 
 // Chance will be 1/n percentage chance of being awarded
-function shouldMessageBeAwarded(authorID, chance = 30) {
+function shouldMessageBeAwarded(chance = 30) {
 	let chances = new Array(chance - 1).fill(0);
 	chances.push(1);
 	chances = arrayShuffle(chances);
-	if (Math.floor(Math.random() * chances.length) == 1) {
-		//TRANSACTIONS.push({ id, amount: 0.1 });
-	}
+	return Math.floor(Math.random() * chances.length) == 1;
 }
