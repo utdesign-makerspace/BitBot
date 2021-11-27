31ed2145e85d6a0dcd23180b068c53948ae49dbe
Cameron Steele <cam.steeleis@gmail.com>
2021-11-27 14:06:31 -0600

    Added buy functionality to redeem button

--- buttons/redeem.js
+++ buttons/redeem.js
@@ -1,4 +1,5 @@
 const constants = require('../lib/constants');
+const { buyItem } = require('../lib/store');
 
 module.exports = {
 	id: 'redeem',
@@ -10,10 +11,10 @@ module.exports = {
 		const reward = constants.rewards[rewardId];
 
 		// TODO: Check if user can afford the reward with bits. Then create a transaction in the database.
-
+		await buyItem(reward.id, interaction.author.id);
 		// Tell user they have redeemed a reward
 		await interaction.editReply({
-			content: `Your ${reward.name} has been redeemed. Please pick it up as soon as possible if required.`,
+			content: `Your ${reward.Title} has been redeemed. Please pick it up as soon as possible if required.`,
 			embeds: [],
 			components: []
 		});
