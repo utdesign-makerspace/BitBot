93cab16f0dcfa8843d9a66d858877ebad20c5e56
Hudson Samuels <hudson.samuels@gmail.com>
2022-02-21 19:22:59 -0600

    Events bug fix
    - Removed /updateevents test command
    - Changed updateCalendar job to only run on specified guild

--- jobs/updateCalendar.js
+++ jobs/updateCalendar.js
@@ -2,11 +2,9 @@ const calendar = require('../lib/calendar.js');
 module.exports = {
 	cron: '0 0 * * * *',
 	action: async (client) => {
-		const guilds = [...client.guilds.cache.values()];
-		console.log(guilds);
-		for (let i = 0; i < guilds.length; i++) {
-			console.log('Updating calendar for ' + guilds[i].name);
-			await calendar.updateDiscordEvents(guilds[i]);
-		}
+		const guild = client.guilds.cache.get(process.env.GUILD_ID);
+		console.log(guild);
+		console.log(`- Updating calendar for ${guild.name}... -`);
+		await calendar.updateDiscordEvents(guild);
 	}
 };
