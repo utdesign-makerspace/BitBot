c5453c8fa728164206e9a6bb83ed575e7a5b0d85
EMERALD <hudson.samuels@gmail.com>
2022-11-09 15:08:55 -0600

    Calendar fix, LDAP only in production

--- jobs/updateCalendar.js
+++ jobs/updateCalendar.js
@@ -1,9 +1,9 @@
 const calendar = require('../lib/calendar.js');
 module.exports = {
 	cron: '0 0 * * * *',
+	runOnStart: true,
 	action: async (client) => {
 		const guild = client.guilds.cache.get(process.env.GUILD_ID);
-		console.log(guild);
 		console.log(`- Updating calendar for ${guild.name}... -`);
 		await calendar.updateDiscordEvents(guild);
 	}
